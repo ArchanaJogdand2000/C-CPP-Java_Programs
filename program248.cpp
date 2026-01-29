2f8fab8f354a8d1b408c3ba9463355aec29e74f4
ArchanaJogdand2000 <archanajogdand24@gmail.com>
2026-01-29 21:31:38 +0530

    Add files via upload

---
+++ program248.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    int iRet = 0;
+    char Arr[30] = "Hello World";
+
+    iRet = strlen(Arr);
+
+    cout<<"String length is : "<<iRet<<endl;
+    
+    return 0;
+}
+
+
+/* c = strlen()
+
+   c++ = strlen(),   .length()
+
+   java = .length()
+
+
+
+*/
